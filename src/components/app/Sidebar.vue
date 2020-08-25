bd47bfa1e3facd4ae5366aa1e685b27359b8df99
medukr <andrii.demydyuk@gmail.com>
2020-08-25 23:10:29 +0300

    l3

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{'open':value}">
+    <router-link
+        v-for="link in links"
+        :key="link.to.name"
+        tag="li"
+        active-class="active"
+        :to="link.to"
+        :exact="link.exact"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+
+
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Счет</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">История</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
+    </li>
+    <li>
+      <a href="#" class="waves-effect waves-orange pointer">Категории</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props:['value'],
+  data: () => ({
+    links: [
+      {title: 'Счет', url: '', to: {name: 'Home'}, exact: true},
+      {title: 'История', url: '', to: {name: 'History'}},
+      {title: 'Планирование', url: '', to: {name: 'Planning'}},
+      {title: 'Новая запись', url: '', to: {name: 'Record'}},
+      {title: 'Категории', url: '', to: {name: 'Categories'}}
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: {layout:'main'},
     component: Home
   },
   {
