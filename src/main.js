e5b84eb50343cb76b5b47e9b6e873366a42d5a85
medukr <andrii.demydyuk@gmail.com>
2020-08-27 00:14:02 +0300

    l5

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import 'materialize-css/dist/js/materialize.min'
+
+import Vuelidate from "vuelidate"; //простая библиотека для валидации полей формы
+
 import dateFilter from "@/filters/date.filter";
+
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuelidate)
+
 Vue.filter('dateFilter', dateFilter); //регистрируем свой фильтр в приложение, называем dateFilter
+
 new Vue({
   router,
   store,
